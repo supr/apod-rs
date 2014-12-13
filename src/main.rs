7aac1d364f7d7dd457fc83cd855c87525131771a
Prudhvi Krishna Surapaneni <prudhvi@aldebaran.supr.io>
2014-12-13 15:38:51 -0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ static APOD_BASE_URL: &'static str = "http://apod.nasa.gov/apod/";
 struct MemoryPage
 {
     code: uint,
-    headers: http::Headers,
     body: Vec<u8> 
 }
 
@@ -36,7 +35,7 @@ impl<'a> Apod<'a> {
         let resp = self.handle.borrow_mut().get(url)
                     .header("User-Agent", "apod-rs/0.1 github.com/supr/apod-rs")
                     .exec().unwrap();
-        MemoryPage { code: resp.get_code(), headers: resp.get_headers().clone(), body: resp.move_body() }
+        MemoryPage { code: resp.get_code(), body: resp.move_body() }
     }
 
     fn get_image_url<'a>(&self, page: &MemoryPage) -> Option<&'a str> {
@@ -50,21 +49,22 @@ impl<'a> Apod<'a> {
         }
     }
 
-    fn download_page(&self, to: String, url: String) -> String {
+    fn download_page(&self, to: String, url: String) -> Option<String> {
         let page = self.get_page(url.as_slice());
         let web_path = Path::new(url);
         let file_name = web_path.filename().unwrap();
 
         let mut file = File::create(&Path::new(format!("{}/{}", to, str::from_utf8(file_name).unwrap())));
-        file.write(page.body.as_slice_());
-
-        format!("{}/{}", to, str::from_utf8(file_name).unwrap())
+        match file.write(page.body.as_slice_()) {
+            Err(_) => None,
+            Ok(_) => Some(format!("{}/{}", to, str::from_utf8(file_name).unwrap()))
+        }
     }
 
     fn set_wallpaper(&self, file: String) {
         let file_path = format!("file://{}", file);
         let args = vec!["set", "org.gnome.desktop.background", "picture-uri", file_path.as_slice()];
-        Command::new("gsettings").args(args.as_slice_()).spawn();
+        let _ = Command::new("gsettings").args(args.as_slice_()).spawn();
     }
 }
 
@@ -72,8 +72,14 @@ fn main() {
     let apod = Apod::new();
     let page = apod.get_page(APOD_BASE_URL);
 
-    if let Some(url) = apod.get_image_url(&page) {
-        let downloaded_file = apod.download_page(format!("{}/Pictures", os::homedir().unwrap().display()), format!("{}{}", APOD_BASE_URL, url));
-        apod.set_wallpaper(downloaded_file);
+    match page.code {
+        200...399 => {
+            if let Some(url) = apod.get_image_url(&page) {
+                if let Some(downloaded_file) = apod.download_page(format!("{}/Pictures", os::homedir().unwrap().display()), format!("{}{}", APOD_BASE_URL, url)) {
+                    apod.set_wallpaper(downloaded_file);
+                }
+            }
+        },
+        _ => { println!("Unable to get APOD Page: {} Status Code: {}", APOD_BASE_URL, page.code); }
     }
 }
